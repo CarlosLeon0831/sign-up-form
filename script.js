6bad69ef9bc0366687eb1934c06aaa1d019a71bc
Carlos <carlosleon0831@gmail.com>
2022-09-05 19:13:45 -0500

    Add message when passwords do not match

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const password = document.querySelector("#password");
-const confirmPassword = document.querySelector("#confirm-password");
+const passOne = document.querySelector("#password");
+const passTwo = document.querySelector("#confirm-password");
 const createAccount = document.querySelector(".account__button");
 const error = document.querySelector("#errorMsg");
 
-if (password.textContent != confirmPassword.textContent) {
-  error.textContent.add("Passwords do not match");
+function checkPassword() {
+  error.innerText =
+    passOne.value == passTwo.value ? "" : "Passwords do not match";
 }
+
+if (checkPassword == false) {
+    passOne.classList.add('error');
+    passTwo.classList.add('error');
+}
+
+passOne.addEventListener('keyup', () => {
+    if (passTwo.value.length != 0) checkPassword();
+})
+
+passTwo.addEventListener('keyup', checkPassword);
+
+
